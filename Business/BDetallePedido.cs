1778d012ef8058aa677ace59fcac4b90abfc6dec
cristhian Lopez <cristhian.lopez@tecsup.edu.pe>
2021-10-03 14:18:18 -0500

    first commit

---
+++ Business/BDetallePedido.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+using Entity;
+
+namespace Business
+{
+    public class BDetallePedido
+    {
+        private DDetallePedido DDetallePedido = null;
+        public List<DetallePedido> GetDetallePedidosPorId(int IdPedido)
+        {
+            List<DetallePedido> detallePedidos = null;
+            try
+            {
+                DDetallePedido = new DDetallePedido();
+                detallePedidos = DDetallePedido.GetDetallePedidos(new DetallePedido { Pedido = new Pedido { IdPedido = IdPedido } });
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                DDetallePedido = null;
+            }
+            return detallePedidos;
+        }
+
+        public decimal GetDetalleTotalPorId(int IdPedido)
+        {
+            List<DetallePedido> detalle_De_Pedidos = null;
+            decimal total = 0;
+            try
+            {
+                DDetallePedido = new DDetallePedido();
+                detalle_De_Pedidos = DDetallePedido.GetDetallePedidos(new DetallePedido { Pedido = new Pedido { IdPedido = IdPedido } });
+
+                foreach (var item in detalle_De_Pedidos)
+                {
+                    total = total + item.Cantidad * item.PrecioUnidad - item.Descuento;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                DDetallePedido = null;
+            }
+            return total;
+        }
+    }
+}
---
+++ Lab7/ViewModel/ListaCategoriaViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+using System.Windows;
+using Entity;
+
+namespace Lab7.ViewModel
+{
+    public class ListaCategoriaViewModel : ViewModelBase
+    {
+        public ObservableCollection<Categoria> Categorias { get; set; }
+        public ICommand NuevoCommand { set; get; }
+        public ICommand ConsultarCommand { set; get; }
+        public ListaCategoriaViewModel()
+        {
+            Categorias = new Model.CategoriaModel().Categorias;
+            NuevoCommand = new RelayCommand<Window>(
+                param=>Abrir()
+                );
+            ConsultarCommand = new RelayCommand<object>(
+                o => { Categorias = (new Model.CategoriaModel()).Categorias; }
+                );
+            void Abrir()
+            {
+                ManCategoria window = new ManCategoria();
+                window.ShowDialog();
+            }
+        }
+    }
+}
